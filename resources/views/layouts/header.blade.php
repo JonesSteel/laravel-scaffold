c68939b8bde4c094734bfbea10e06e385caba89c
Eduardo Juarez <gjuarez9@gmail.com>
2020-08-03 21:02:26 -0500

    First Commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<header class="app-header navbar">
+    <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img class="navbar-brand-full" src="svg/modulr.svg" width="89" height="25" alt="Modulr Logo">
+        <img class="navbar-brand-minimized" src="svg/modulr-icon.svg" width="30" height="30" alt="Modulr Logo">
+    </a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar"
+</header>
